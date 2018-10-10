49c85af15ddef47dee8e01274b2b8bbc61df764c
Jason Robert <jason@grandcircus.co>
2018-10-10 18:00:11 -0400

    Class 1 Examples

---
+++ Ahbc.Class.One/Ahbc.Class.One/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahbc.Class.One
+{
+    class Program
+    {
+        /// <summary>
+        /// My main method that will show when referenced
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            const string WelcomeText = "Hi.  Whats your name?";
+
+            // Asking for name
+            Console.WriteLine(WelcomeText);
+            string name = Console.ReadLine();
+
+            /* Writing 
+             * Name */
+            Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
