e582e1925ec05871bd00f299e819ab89f326ff91
Jason Robert <jason@grandcircus.co>
2018-12-17 21:50:15 -0500

    Class 31

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,19 +1,42 @@
 var temp = 2;
 var tempstring = "2";
 
-// hello
-if (temp == tempstring) {
-    
-} 
-
 var _count = 0;
 var element = document.getElementById("output");
 function increment() {
-    _count++;
+    var incrementby = prompt("How many would you like to increment by?");
+    _count += incrementby;
     element.innerText = _count;
+    alert("You have incremented!");
 }
 
 function decrement() {
     _count--;
     element.innerText = _count;
 }
+
+///// Creating elements with the DOM /////////
+
+var sample = document.getElementById("sample");
+//sample.innerText = "<div>hello</div>";
+//sample.innerHTML = "<div>hello</div>";
+
+var something = document.createElement("span");
+something.innerText = "Zack's method!!!";
+sample.appendChild(something);
+
+//var globalVar = "Heyyyy youuuuu guuuuuuuyss";
+$(document).ready(function () {
+    
+    // All divs - $("div")
+    $("div:even").css("color", "red").click(function () {
+        alert("clicked!");
+    });
+
+    // Sample by id $("#sample")
+
+});
+
+(function(){
+    // self invoking anonomous function
+})();
