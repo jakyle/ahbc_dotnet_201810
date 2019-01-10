b6597c361cb8cd65ba17f59c9e8cd3c8b1b2e654
Jason Robert <jason@grandcircus.co>
2019-01-09 21:42:06 -0500

    ASP.NET MVC

---
+++ Ahbc.Class.FortyFull/Ahbc.Class.FortyFull/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using Ahbc.Class.FortyFull.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ahbc.Class.FortyFull.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index(int? id)
+        {
+            var model = new DemoInputModel
+            {
+                FirstName = "Jason",
+                MiddleName = "Timothy",
+                LastName = "Robert",
+                IsActive = true
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Ahbc.Class.FortyFull/Ahbc.Class.FortyFull/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ahbc.Class.FortyFull.MvcApplication" Language="C#" %>
