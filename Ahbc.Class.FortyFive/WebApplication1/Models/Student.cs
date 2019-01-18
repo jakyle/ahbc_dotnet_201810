dc3a36b384eb4aeb0fdab3c558e1555212ffe30e
Jason Robert <jason@grandcircus.co>
2019-01-17 21:10:22 -0500

    EF Part 1

---
+++ Ahbc.Class.FortyFive/WebApplication1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
